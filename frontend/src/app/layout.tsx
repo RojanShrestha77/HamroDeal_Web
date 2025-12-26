66abe06b86f7c03fca133e80569ef5e16ba2d86e
RojanShrestha77 <shresrozan555@gmail.com>
2025-12-26 08:07:27 +0545

    Sprint 1

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google"; // or Poppins
 import "./globals.css";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
 
 const poppins = Inter({ subsets: ["latin"], variable: "--font-poppins" }); // Replace with Poppins if imported
 
@@ -16,7 +18,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${poppins.variable} font-poppins`}>{children}</body>
+      <body className={`${poppins.variable} font-poppins`}>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
