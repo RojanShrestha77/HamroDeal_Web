4019bcf413ae337b7c6731546c2eb11e370c31f4
RojanShrestha77 <shresrozan555@gmail.com>
2026-03-06 15:43:24 +0545

    remvoed duplicate code

--- backend/src/routes/seller/seller.route.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { SellerController } from "../../controllers/seller/seller.controller";
-
-let sellerController = new SellerController();
-
-const router = Router();
-
-router.post("/",sellerController.createSeller);
-export default router;
